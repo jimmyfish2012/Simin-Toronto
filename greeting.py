fa62b1bd4609ee43f38122b1034cec7ce3cc8790
Simin Yu <siminyu2009@gmail.com>
2019-09-27 15:15:26 -0400

    my second attempt

---
+++ greeting.py
@@ -0,0 +1,6 @@
+# Author: Simin, Yu
+# Date: 27/09/2019
+# Description: put people's name as input and print a greeting message
+
+person_name = input("Please enter your name: ")
+print ("Hello " + person_name+"!")
